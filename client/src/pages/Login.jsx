59571dddc010eb6210f843e41b92618af3dacc65
Antim <antimsarker0@gmail.com>
2024-07-17 20:52:00 +0600

    login page

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div className="hero bg-base-200 min-h-screen flex items-center justify-center">
+      <div className="card bg-base-100 w-full max-w-md shadow-2xl">
+        <form className="card-body">
+          <h1 className="text-center font-semibold text-2xl mb-4">Register Now</h1>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Name</span>
+            </label>
+            <input type="text" placeholder="name" className="input input-bordered" required />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input type="email" placeholder="email" className="input input-bordered" required />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input type="password" placeholder="password" className="input input-bordered" required />
+            <label className="label">
+              <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+            </label>
+          </div>
+          <div className="form-control mt-6">
+            <button className="btn btn-primary w-full">Submit</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default Login
