4d5e2b61c646a15673582555de6db126a262ffb7
Antim <antimsarker0@gmail.com>
2024-07-25 20:30:40 +0600

    userRoute setup

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,28 +1,41 @@
 import React from 'react'
 import {Link} from 'react-router-dom';
 function Register() {
+  const submitForm=(e)=>{
+    e.preventDefault();
+    const form=e.target;
+    const name=form.name.value;
+    const email=form.email.value;
+    const password=form.password.value;
+    const user={
+      "name":name,
+      "email":email,
+      "password":password
+    }
+    console.log(user)
+  }
   return (
     <div className="hero bg-base-200 min-h-screen flex items-center justify-center">
       <div className="card bg-base-100 w-full max-w-md shadow-2xl">
-        <form className="card-body">
+        <form onSubmit={submitForm} className="card-body">
           <h1 className="text-center font-semibold text-2xl mb-4">Register Now</h1>
           <div className="form-control">
             <label className="label">
               <span className="label-text">Name</span>
             </label>
-            <input type="text" placeholder="name" className="input input-bordered" required />
+            <input type="text" name='name' placeholder="name" className="input input-bordered" required />
           </div>
           <div className="form-control">
             <label className="label">
               <span className="label-text">Email</span>
             </label>
-            <input type="email" placeholder="email" className="input input-bordered" required />
+            <input type="email" name='email' placeholder="email" className="input input-bordered" required />
           </div>
           <div className="form-control">
             <label className="label">
               <span className="label-text">Password</span>
             </label>
-            <input type="password" placeholder="password" className="input input-bordered" required />
+            <input type="password" name='password' placeholder="password" className="input input-bordered" required />
             <label className="label">
               <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
             </label>
--- server/config/dbConfig.js
+++ server/config/dbConfig.js
@@ -1,10 +1,19 @@
-const mongoose=require('mongoose');
-mongoose.connect(process.env.MONGOURL);
-const connection=mongoose.connection;
-connection.on("connected",()=>{
-    console.log('mongodb is coonected')
-})
-connection.on('error',()=>{
-    console.log('error')
-})
-module.exports=mongoose;
+const mongoose = require('mongoose');
+require('dotenv').config();
+const MONGO_URL=process.env.MONGOURL
+mongoose.connect(MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const connection = mongoose.connection;
+
+connection.on('connected', () => {
+  console.log('MongoDB is connected');
+});
+
+connection.on('error', (err) => {
+  console.error(`MongoDB connection error: ${err.message}`);
+});
+
+module.exports = mongoose;
