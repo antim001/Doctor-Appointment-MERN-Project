bc9af55836247488024c38446443863a553219c7
Antim <antimsarker0@gmail.com>
2024-11-04 22:48:19 +0600

    server site added to config

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
-export const BASE_URL='http://localhost:5000/api/v1' 
+export const BASE_URL='https://doctor-appointment-mern-project-backend.vercel.app/api/v1' 
 export const token =localStorage.getItem("token");
